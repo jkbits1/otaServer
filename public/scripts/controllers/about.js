1685f5e119fbe8fd6def01e5dc7331dbc8e11012
jkbits1 <jkbits1@users.noreply.github.com>
2015-09-25 23:53:05 +0100

    included angular client

---
+++ public/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name otaClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the otaClientApp
+ */
+angular.module('otaClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ public/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name otaClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the otaClientApp
+ */
+angular.module('otaClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
